1d68ab539de96f0c4b7f56c4f8a29ee10878d7ac
Cotton Top <mtmcgr@gmail.com>
2019-11-19 12:26:43 -0600

    separated javascript and questions files

--- questions.js
+++ questions.js
@@ -1,25 +1,26 @@
-{
-    title: "Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-            answer: "alerts"
-},
-{
-    title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-            answer: "parentheses"
-},
-{
-    title: "Arrays in javascript can be used to store ____.",
-        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-            answer: "all of the above"
-},
-{
-    title: "String values must be closed within ____ when being assigned to variables.",
-        choices: ["commas", "curly brackets", "quotes", "parentheses"],
-            answer: "quotes"
-},
-{
-    title: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        choices: ["Javascript", "terminal/bash", "for loops", "console.log"],
-            answer: "console.log"
-},
+
+//     title: "Commonly used data types DO NOT include:",
+//         choices: ["strings", "booleans", "alerts", "numbers"],
+//             answer: "alerts"
+// },
+// {
+//     title: "The condition in an if / else statement is enclosed within ____.",
+//         choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+//             answer: "parentheses"
+// },
+// {
+//     title: "Arrays in javascript can be used to store ____.",
+//         choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+//             answer: "all of the above"
+// },
+// {
+//     title: "String values must be closed within ____ when being assigned to variables.",
+//         choices: ["commas", "curly brackets", "quotes", "parentheses"],
+//             answer: "quotes"
+// },
+// {
+//     title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+//         choices: ["Javascript", "terminal/bash", "for loops", "console.log"],
+//             answer: "console.log"
+// },
+// {
---
+++ script.js
@@ -0,0 +1,13 @@
+function initGame(){
+    const startButtonEL = document.querySelector("button");
+    console.log("button");
+    const startContainerEl = document.getElementById("start-container");
+    console.log("start");
+    
+    function startGame(){
+    
+        startContainerEl.setAttribute("class", "container d-none")
+    }
+    
+    }
+    initGame();
