dc6253e093fd6479956cc4891316d720a92cf945
Cotton Top <mtmcgr@gmail.com>
2019-11-19 13:01:14 -0600

    updated javascript for game and start containers

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ function initGame(){
     console.log("button");
     const startContainerEl = document.getElementById("start-container");
     console.log("start");
+    const gameContainerEl = document.getElementById("game-container");
     
     function startGame(){
     
-        startContainerEl.setAttribute("class", "container d-none")
+        startContainerEl.setAttribute("class", "container d-none");
+        gameContainerEl.setAttribute("class", "container");
     }
     
+    startButtonEL.addEventListener ("click", startGame);
+
     }
     initGame();
