592da807fb5e39351324cad1d611918d03ab11f9
Cotton Top <mtmcgr@gmail.com>
2019-11-19 22:33:25 -0600

    added to javascript with tutor help

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let currentQuestionIndex = 0
+
 function initGame(){
     const startButtonEL = document.querySelector("button");
     console.log("button");
@@ -9,9 +11,22 @@ function initGame(){
     
         startContainerEl.setAttribute("class", "container d-none");
         gameContainerEl.setAttribute("class", "container");
+        // getQuestion()
+        
     }
     
     startButtonEL.addEventListener ("click", startGame);
 
     }
+function getQuestion(){
+//
+const currentQuestion = questions[currentQuestionIndex];
+// display title of current question in div on HTML page
+// loop over choices and for each element in choices, create a button
+// add to button: function "on click" : if choice matches, vs if choice doesn't match
+// different function: then call next question
+
+}
+
+
     initGame();
